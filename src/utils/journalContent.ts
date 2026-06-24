c39a6205de15199769b446391b17eaf8805a4189
Dean-Baylem <dbaylem5822@gmail.com>
2026-06-24 13:24:24 +0900

    Created Journal archive page. Finished the projects archvie page. Refactored the hero section

---
+++ src/utils/journalContent.ts
@@ -0,0 +1,28 @@
+
+export type JournalEntry = {
+    title: string;
+    date: string;
+    datetime: string;
+    link: string;
+};
+
+export const journalEntries: JournalEntry[] = [
+    {
+        title: "Implementing helper functions for the WordPress Block Editor",
+        date: "June 17, 2026",
+        datetime: "2026-06-17",
+        link: "/journal/implementing-helper-functions-for-the-wordpress-block-editor",
+    },
+    {
+        title: "Implementing helper functions for the WordPress Block Editor",
+        date: "June 17, 2026",
+        datetime: "2026-06-17",
+        link: "/journal/implementing-helper-functions-for-the-wordpress-block-editor",
+    },
+    {
+        title: "Implementing helper functions for the WordPress Block Editor",
+        date: "June 17, 2026",
+        datetime: "2026-06-17",
+        link: "/journal/implementing-helper-functions-for-the-wordpress-block-editor",
+    },
+];
---
+++ src/utils/projectContent.ts
@@ -0,0 +1,86 @@
+export type Display = {
+    heading: string;
+    title: string;
+    description: string;
+    image: {
+        src: string;
+        alt: string;
+        height: string;
+        width: string;
+    };
+    skills: string[];
+    links: {
+        text: string;
+        href: string;
+        target: string;
+        type: string;
+    }[];
+};
+
+export const workDisplays = [
+    {
+        heading: "Recent Project",
+        title: "Dungeon Delvers Inc.",
+        description: "A worldbuilding and campaign management tool for TTRPG’s. Created with Next.js, using Payload CMS for content management.",
+        image: {
+            src: "./castle.webp",
+            alt: "Castle Icon",
+            height: "159",
+            width: "159",
+        },
+        skills: ["Next.js", "Payload", "TypeScript", "Leaflet.js", "Tailwind"],
+        links: [
+            {
+                text: "Live Site",
+                href: "https://dungeon-delvers-inc-payload.vercel.app/",
+                target: "_self",
+                type: "primary",
+            },
+            {
+                text: "Summary",
+                href: "/projects/dungeon-delvers-inc",
+                target: "_self",
+                type: "secondary",
+            },
+            {
+                text: "Github Repo",
+                href: "https://github.com/Dean-Baylem/dungeon-delvers-inc-payload",
+                target: "_blank",
+                type: "secondary",
+            },
+        ],
+    },
+    {
+        heading: "Latest Experiment",
+        title: "Campfire Scene",
+        description:
+            "Stylized 3D environment inspired by early web games. Built using a combination of Blender, React Three Fiber, and custom GLSL shaders.",
+        image: {
+            src: "./campfire.webp",
+            alt: "Campfire Icon",
+            height: "140",
+            width: "159",
+        },
+        skills: ["React", "Three.js", "React Three Fiber", "GLSL", "Blender"],
+        links: [
+            {
+                text: "Live Site",
+                href: "https://druid-camp.netlify.app/",
+                target: "_self",
+                type: "primary",
+            },
+            {
+                text: "Summary",
+                href: "/experiments/campfire-scene",
+                target: "_self",
+                type: "secondary",
+            },
+            {
+                text: "Github Repo",
+                href: "https://github.com/Dean-Baylem/campfire-scene",
+                target: "_blank",
+                type: "secondary",
+            },
+        ],
+    },
+];
